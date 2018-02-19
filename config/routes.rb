7ba3d1e5357edc864ac4236959271fdf5e482acc
“Lawrence <lawrence.ly667@gmail.com>
2018-02-20 00:31:49 +1100

    gems, views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #             Prefix Verb   URI Pattern                                       Controller#Action
+  root to: 'pages#home'
+
+  #login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+
+  #users
+  get '/users/edit' => 'users#edit', :as => :edit_user
+
+  #routes
+  resources :users, :pages, :accounts, :except => [:edit]
 end
